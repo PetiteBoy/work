7474e7b81783e35b253c84ad626771b40a35b6e3
wangrunzhuang <wangrunzhuang@zuoyebang.com>
2020-08-31 15:44:58 +0800

    '1'

---
+++ view/dist/precache-manifest.e230867e51538e937c05d81f47d25d49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7afcdf70351458dc12f",
+    "url": "/css/app.6cdb0a08.css"
+  },
+  {
+    "revision": "72415d4f48006de32d6dc04f71ca2b1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7afcdf70351458dc12f",
+    "url": "/js/app.efde647f.js"
+  },
+  {
+    "revision": "e68e7160cfc2305db1a4",
+    "url": "/js/chunk-vendors.3a08c2a5.js"
+  },
+  {
+    "revision": "0a59249127144d5e75d9ee93c9d2b959",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
